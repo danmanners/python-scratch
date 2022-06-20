bbf1125ddb7a2aa0183e2878b096ea6d30cf8d7e
Dan Manners <daniel.a.manners@gmail.com>
2022-06-20 13:17:42 -0400

    Optimizations for Dictionary and getting random number OR word.

--- app.py
+++ app.py
@@ -5,30 +5,54 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# def downloadDictionary()
+dictionaryData = list()
+
+
+@app.route('/dict/refresh')
+# When called, download the dictionary
+def downloadDictionary(dictionary='https://github.com/dwyl/english-words/raw/master/words.txt'):
+    http = urllib3.PoolManager()
+    r = http.request('GET', dictionary)
+
+    if r.status == 200:
+        data = (r.data).decode('UTF-8')
+        # Update the list GLOBALLY
+        global dictionaryData
+        dictionaryData = data.splitlines()
+        return f'Dictionary refreshed!\n'
+    else:
+        return f'The dictionary URL was unavailable!'
+
 
 def randomNumber(start=0, end=1000, step=1):
     value = random.randrange(start, end, step)
     return value
 
 
+@app.route('/random')
+def randomObject():
+    selector = randomNumber(start=1, end=3)
+    if str(selector) == '1':
+        return randomWord()
+    if str(selector) == '2':
+        randomInt = f'{randomNumber()}\n'
+        return randomInt
+
+
 @app.route('/random/word')
-def randomWord(dictionary='https://github.com/dwyl/english-words/raw/master/words.txt'):
+def randomWord():
 
-    http = urllib3.PoolManager()
-    r = http.request('GET', dictionary)
-
-    if r.status == 200:
-        data = (r.data).decode('UTF-8')
-        lineCount = len(data.split('\n'))
-        lineNumber = randomNumber(start=0, end=lineCount)
-        return str(data.split("\n")[lineNumber] + "\n")
-
-    else:
-        return f'The dictionary URL was unavailable!'
+    lineCount = len(dictionaryData)
+    lineNumber = randomNumber(start=0, end=lineCount)
+    return str(dictionaryData[lineNumber] + "\n")
 
 
 @app.route('/random/number')
 def random_route():
     number = randomNumber()
-    return f'{number}\n'
+    return number
+
+
+if __name__ == '__main__':
+    downloadDictionary()
+    app.run(host='0.0.0.0', port=5000)
